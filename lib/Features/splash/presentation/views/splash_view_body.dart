e7c5645884f44e6ab4db5285cbe4403c6a9ce3fe
Ali-2152004 <112330406+Ali-2152004@users.noreply.github.com>
2025-08-01 16:18:23 +0300

    finish the animated splash screen

--- lib/Features/splash/presentation/views/splash_view_body.dart
+++ lib/Features/splash/presentation/views/splash_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashViewBody extends StatelessWidget {
@@ -11,13 +12,30 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        SvgPicture.asset(AssetsData.logo, height: 60),
-        SizedBox(height: 12),
-        Text(
-          textAlign: TextAlign.center,
-          'Read free books easily',
-          style: TextStyle(),
-        ),
+        SvgPicture.asset(
+          AssetsData.logo,
+          height: 60,
+        ).animate().fadeIn(duration: 1.5.seconds, delay: 0.5.seconds),
+        SizedBox(height: 18),
+        const Text(
+              textAlign: TextAlign.center,
+              'Read free books easily',
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'Edu',
+                wordSpacing: 2,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+            .animate()
+            .moveY(
+              delay: 1.seconds,
+              duration: 1.5.seconds,
+              curve: Curves.easeInOut,
+              begin: 30,
+              end: 0,
+            )
+            .fadeIn(delay: 1.seconds),
       ],
     );
   }
