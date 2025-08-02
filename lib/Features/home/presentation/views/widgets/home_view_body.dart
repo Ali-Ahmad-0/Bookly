d7029e2c46762ad9cd26f8135ba92f34ef0d478a
Ali-2152004 <112330406+Ali-2152004@users.noreply.github.com>
2025-08-02 04:52:35 +0300

    create const app styles

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,9 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_appbar.dart';
-import 'package:bookly/Features/home/presentation/views/widgets/custom_listView_item.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featured_listview.dart';
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/style.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -14,16 +13,59 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: [
         CustomAppBar(),
-        FeaturedLIstView(),
-        Padding(
-          padding: const EdgeInsets.only(left: 24, top: 32, bottom: 32),
-          child: SizedBox(
-            height: 50,
-            width: double.infinity,
-            child: Text('Best seller ', style: Styles.textMedium),
+        Expanded(
+          child: ListView(
+            children: [
+              FeaturedLIstView(),
+              Padding(
+                padding: const EdgeInsets.only(left: 24, top: 42),
+                child: SizedBox(
+                  height: 40,
+                  child: Text('Best seller', style: Styles.textstyle18),
+                ),
+              ),
+
+              BestsellerListViewItem(),
+            ],
           ),
         ),
       ],
+    );
+  }
+}
+
+class BestsellerListViewItem extends StatelessWidget {
+  const BestsellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 160,
+      child: Row(
+        children: [
+          Container(
+            height: 160,
+            width: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(18),
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  'assets/images/harry-potter-and-the-chamber-of-secrets-6.png',
+                ),
+              ),
+            ),
+          ),
+          Text(
+            textAlign: TextAlign.left,
+            'Harry Potter and the Chamber of Secrets',
+            style: TextStyle(
+              fontSize: 16,
+              fontFamily: GoogleFonts.spectral().fontFamily,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/core/utils/style.dart
+++ lib/core/utils/style.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 abstract class Styles {
-  static const textMedium = TextStyle(
-    fontSize: 24,
+  static const textstyle18 = TextStyle(
+    fontSize: 18,
     fontWeight: FontWeight.w600,
+  );
+  static TextStyle textstyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: GoogleFonts.spectral().fontFamily,
+  );
+  static const textstyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  static const textstyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle textstyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w500,
+    fontFamily: GoogleFonts.spectral().fontFamily,
   );
 }
