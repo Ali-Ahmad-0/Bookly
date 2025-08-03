ee50d1a61a507b00e8d5e36bb24ff42086851412
Ali-2152004 <112330406+Ali-2152004@users.noreply.github.com>
2025-08-03 23:26:00 +0300

    Add search view route to app router for improved navigation

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:bookly/Features/home/presentation/views/book_details_view.dart';
 import 'package:bookly/Features/home/presentation/views/home_view.dart';
+import 'package:bookly/Features/search_view/presentation/views/search_view.dart';
 import 'package:bookly/Features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
 static const String khomeview = '/homeview';
 static const String kBookDetails = '/bookdetails';
+static const String ksearch = '/searchView';
   
 static final   router = GoRouter(
   routes: [
@@ -23,6 +25,11 @@ static final   router = GoRouter(
     GoRoute(
       path: kBookDetails,
       builder: ( context,  state) => const BookDetailsView()
+    
+    ),
+    GoRoute(
+      path: ksearch,
+      builder: ( context,  state) => const SearchView()
     
     ),
   ],
