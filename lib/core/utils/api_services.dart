c25ab9e73f5ce508c3b5a13050eb8584b86ce43c
Ali-2152004 <112330406+Ali-2152004@users.noreply.github.com>
2025-08-05 21:28:43 +0300

    create API services

---
+++ lib/core/utils/api_services.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+class ApiServices {
+  final _baseURL = 'https://www.googleapis.com/books/v1/'; // fake link
+
+  final Dio dio;
+  ApiServices({required this.dio});
+
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+   var  response = await dio.get('$_baseURL$endPoint');
+    return response.data;
+  }
+}
