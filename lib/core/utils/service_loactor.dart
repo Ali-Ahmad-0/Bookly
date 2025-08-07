cdbe560ce3dc16cae757cf44ecb7dd5fbcdcb521
Ali-2152004 <112330406+Ali-2152004@users.noreply.github.com>
2025-08-07 22:34:08 +0300

    setup service locator

---
+++ lib/core/utils/service_loactor.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/Features/home/data/repos/home_repo_implementation.dart';
+import 'package:bookly/core/utils/api_services.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiServices>(ApiServices(dio: Dio()));
+  getIt.registerSingleton<HomeRepoImplementation>(
+    HomeRepoImplementation(apiServices: getIt.get<ApiServices>()),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
+import 'package:bookly/Features/home/data/repos/home_repo_implementation.dart';
+import 'package:bookly/Features/home/presentation/manager/features_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly/conistants.dart';
+import 'package:bookly/core/utils/api_services.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_loactor.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const Bookly());
+  setupServiceLocator();
+  // Initialize shared dependencies
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              FeaturedBooksCubit(getIt.get<HomeRepoImplementation>()),
+        ),
+        BlocProvider(
+          create: (context) =>
+              NewestBooksCubit(getIt.get<HomeRepoImplementation>()),
+        ),
+      ],
+      child: const Bookly(),
+    ),
+  );
 }
 
 class Bookly extends StatelessWidget {
@@ -13,12 +37,12 @@ class Bookly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig:  AppRouter.router,
+      routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
-      
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kprimaryColor
-      , textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
-      
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kprimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
     );
   }
 }
