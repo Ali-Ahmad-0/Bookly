98e3ceb8d59ff27e7d27cef0604c2154a4099a29
Ali-2152004 <112330406+Ali-2152004@users.noreply.github.com>
2025-08-30 18:58:49 +0300

    finish the app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:bookly/Features/home/presentation/manager/newest_books_cubit/new
 import 'package:bookly/conistants.dart';
 import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/service_loactor.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -26,7 +28,10 @@ void main() {
         ),
       ],
 
-      child: Bookly(),
+      child: DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => Bookly(),
+      ),
     ),
   );
 }
@@ -37,13 +42,15 @@ class Bookly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      // ignore: deprecated_member_use
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kprimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme,
-        ),
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
     );
   }
